e2d8c0f5c511b0c17cb21af05fdcf341a47a2de3
Claudia <that.claudiaa@gmail.com>
2024-12-08 00:41:25 -0300

    arreglo de rutas y orden de archivos

--- js/iconos.js
+++ js/iconos.js
@@ -1,144 +1,144 @@
 // Iconos
 var icon = L.icon({
-    iconUrl: "../img.png",
+    iconUrl: "./fwdiconografamapatortel/img.png",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icAbarrotes = L.icon({
-    iconUrl: "../fwdiconografamapatortel/abarrotes.svg",
+    iconUrl: "./fwdiconografamapatortel/abarrotes.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icRegistro = L.icon({
-    iconUrl: "../fwdiconografamapatortel/registro.svg",
+    iconUrl: "./fwdiconografamapatortel/registro.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icBiblioteca = L.icon({
-    iconUrl: "../fwdiconografamapatortel/biblioteca.svg",
+    iconUrl: "./fwdiconografamapatortel/biblioteca.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icAlimentacion = L.icon({
-    iconUrl: "../fwdiconografamapatortel/alimentacion.svg",
+    iconUrl: "./fwdiconografamapatortel/alimentacion.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icAlojamiento = L.icon({
-    iconUrl: "../fwdiconografamapatortel/alojamiento.svg",
+    iconUrl: "./fwdiconografamapatortel/alojamiento.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icArtesania = L.icon({
-    iconUrl: "../fwdiconografamapatortel/artesania.svg",
+    iconUrl: "./fwdiconografamapatortel/artesania.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icAventura = L.icon({
-    iconUrl: "../fwdiconografamapatortel/aventura.svg",
+    iconUrl: "./fwdiconografamapatortel/aventura.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icBomberos = L.icon({
-    iconUrl: "../fwdiconografamapatortel/bomberos.svg",
+    iconUrl: "./fwdiconografamapatortel/bomberos.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icCabaña = L.icon({
-    iconUrl: "../fwdiconografamapatortel/cabaña.svg",
+    iconUrl: "./fwdiconografamapatortel/cabaña.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icCama = L.icon({
-    iconUrl: "../fwdiconografamapatortel/cama.svg",
+    iconUrl: "./fwdiconografamapatortel/cama.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icCarabineros = L.icon({
-    iconUrl: "../fwdiconografamapatortel/carabineros.svg",
+    iconUrl: "./fwdiconografamapatortel/carabineros.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icCombustible = L.icon({
-    iconUrl: "../fwdiconografamapatortel/combustible.svg",
+    iconUrl: "./fwdiconografamapatortel/combustible.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icConaf = L.icon({
-    iconUrl: "../fwdiconografamapatortel/conaf.svg",
+    iconUrl: "./fwdiconografamapatortel/conaf.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icEspacio = L.icon({
-    iconUrl: "../fwdiconografamapatortel/espacio publico.svg",
+    iconUrl: "./fwdiconografamapatortel/espacio publico.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icFerreteria = L.icon({
-    iconUrl: "../fwdiconografamapatortel/ferreteria.svg",
+    iconUrl: "./fwdiconografamapatortel/ferreteria.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icInformaciones = L.icon({
-    iconUrl: "../fwdiconografamapatortel/informaciones.svg",
+    iconUrl: "./fwdiconografamapatortel/informaciones.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icMirador = L.icon({
-    iconUrl: "../fwdiconografamapatortel/mirador.svg",
+    iconUrl: "./fwdiconografamapatortel/mirador.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icMunicipalidad = L.icon({
-    iconUrl: "../fwdiconografamapatortel/municipalidad.svg",
+    iconUrl: "./fwdiconografamapatortel/municipalidad.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icOtros = L.icon({
-    iconUrl: "../fwdiconografamapatortel/otros servicios.svg",
+    iconUrl: "./fwdiconografamapatortel/otros servicios.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icPlaza = L.icon({
-    iconUrl: "../fwdiconografamapatortel/plaza.svg",
+    iconUrl: "./fwdiconografamapatortel/plaza.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icPosta = L.icon({
-    iconUrl: "../fwdiconografamapatortel/posta.svg",
+    iconUrl: "./fwdiconografamapatortel/posta.svg",
     iconSize: [30, 35],
     popupAnchor: [0, -10]
 });
 
 var icPlayground = L.icon({
-    iconUrl: "../fwdiconografamapatortel/playground.png",
+    iconUrl: "./fwdiconografamapatortel/playground.png",
     iconSize: [30, 30],
     popupAnchor: [0, -10]
 });
 
 var icRural = L.icon({
-    iconUrl: "../fwdiconografamapatortel/rural.png",
+    iconUrl: "./fwdiconografamapatortel/rural.png",
     iconSize: [30, 30],
     popupAnchor: [0, -10]
 });
